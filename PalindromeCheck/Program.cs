a8368f7f3c8ce8fd9ba0d63a45002e218550801f
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-26 12:36:20 +0530

    Add files via upload

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            Console.WriteLine("Enter a number: ");
+            int number= Convert.ToInt32(Console.ReadLine());
+            int tempNumber = number;
+            while (tempNumber > 0)
+            {
+                value = value * 10;
+                int temp = tempNumber % 10;
+                value=value + temp;
+                
+                tempNumber = tempNumber / 10;
+            }
+
+            if (value == number)
+            {
+                Console.WriteLine("Yes, " +number+ " is a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("No, " +number+ " is not a Palindrome.");
+            }
+        }
+    }
+}
